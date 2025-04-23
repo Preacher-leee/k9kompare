1fb2cb7ae2707aaa83fa2ce4c4518162012520eb
Preacher-leee <travis.gordon727@gmail.com>
2025-04-23 16:44:23 -0400

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Logo from '../ui/Logo.png';
+import Logo from '../ui/Logo';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
